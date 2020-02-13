825b8e39b0c67e3c8eec0b2e5f71a3c7d45637e2
naomi <zarza.naomi@gmail.com>
2020-02-13 12:10:19 +0100

    Vacía clase Dado para cambios

--- src/main/java/pikopikomazorkito/Dado.java
+++ src/main/java/pikopikomazorkito/Dado.java
@@ -11,30 +11,4 @@ package pikopikomazorkito;
  */
 public class Dado {
 
-    //Atributos
-    private int numero;
-    
-    //Método constructor por defecto
-    public Dado() {
-    }
-    
-    //Método constructor parametrizado
-    public Dado(int numero) {
-        this.numero = numero;
-    }
-    
-    //Métodos getters y setters
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-    
-    //Método toString
-    @Override
-    public String toString() {
-        return "Dado{" + "numero=" + numero + '}';
-    }  
 }
