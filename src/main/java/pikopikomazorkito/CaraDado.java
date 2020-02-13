3739323f5f905b8381f449f43d4a4139a997c496
naomi <zarza.naomi@gmail.com>
2020-02-13 12:05:59 +0100

    Añade clase Enum CaraDado

---
+++ src/main/java/pikopikomazorkito/CaraDado.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pikopikomazorkito;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author naomi
+ */
+public enum CaraDado {
+
+    CARA1(1, 1, new ImageIcon("res/mazorko.jpeg")), //Url de la imagen del dado
+    CARA2(2, 2, new ImageIcon("res/mazorko.jpeg")),
+    CARA3(3, 3, new ImageIcon("res/mazorko.jpeg")),
+    CARA4(4, 4, new ImageIcon("res/mazorko.jpeg")),
+    CARA5(5, 5, new ImageIcon("res/mazorko.jpeg")),
+    CARA6(6, 5, new ImageIcon("res/mazorko.jpeg"));
+
+    //Atributos
+    private int numero;
+    private int puntuacion; //Valor (1-5)
+    private ImageIcon imagen;
+
+    //Método constructor parametrizado
+    private CaraDado(int numero, int puntuacion, ImageIcon imagen) {
+        this.numero = numero;
+        this.puntuacion = puntuacion;
+        this.imagen = imagen;
+    }
+    
+    //Métodos getters y setters
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public ImageIcon getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(ImageIcon imagen) {
+        this.imagen = imagen;
+    }
+    
+    //Método toString
+    @Override
+    public String toString() {
+        return "CaraDado{" + "numero=" + numero + ", puntuacion=" + puntuacion + ", imagen=" + imagen + '}';
+    }  
+}
