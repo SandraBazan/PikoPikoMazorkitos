6d387068d9d020860395e260d1d0e87e074dd62e
nao <zarza.naomi@gmail.com>
2020-02-27 13:55:03 +0100

    Añade clase Jugador con cambios y método lanzarDados()

--- src/main/java/pikopikomazorkito/Jugador.java
+++ src/main/java/pikopikomazorkito/Jugador.java
@@ -12,52 +12,37 @@ import java.util.ArrayList;
  * @author naomi
  */
 public class Jugador {
-    
     //Atributos
     private String nombreJugador;
-    private Dado dado1; //El jugador tendrá ocho dados
-    private Dado dado2;
-    private Dado dado3;
-    private Dado dado4;
-    private Dado dado5;
-    private Dado dado6;
-    private Dado dado7;
-    private Dado dado8;
-   
-    private boolean turno;
-    
-    ArrayList<Ficha> ficha; //Fichas acumuladas
-    
-    private int racionMazorkos; //Cantidad de Mazorkos seleccionados
-    private int valorAcumulado; //Valor acumulado de los dados seleccionados
+    Dado[] dados; //Array de dados 
+    private int puntosAcumulados; //Valor acumulado de los dados seleccionados
+    //ArrayList Fichas susRaciones; (aún no disponible)
     
     //Método constructor por defecto
     public Jugador() {
     }
     
-    //Método constructor parametrizado
-    public Jugador(String nombreJugador, Dado dado1, Dado dado2, Dado dado3, Dado dado4, Dado dado5, Dado dado6, Dado dado7, Dado dado8, boolean turno, ArrayList<Ficha> ficha, int racionMazorkos, int valorAcumulado) {
+    //Método constructor parametrizado que recibe el nombre del jugador
+    public Jugador(String nombreJugador) {
         this.nombreJugador = nombreJugador;
-        this.dado1 = dado1;
-        this.dado2 = dado2;
-        this.dado3 = dado3;
-        this.dado4 = dado4;
-        this.dado5 = dado5;
-        this.dado6 = dado6;
-        this.dado7 = dado7;
-        this.dado8 = dado8;
-        this.turno = turno;
-        this.ficha = ficha;
-
-        this.racionMazorkos = racionMazorkos;
-        this.valorAcumulado = valorAcumulado;
+        this.puntosAcumulados = puntosAcumulados;
+        //Instancio la lista de Dados de cada Jugador
+        //y le asigno un Dado a cada posición
+        this.dados = new Dado[8];
+        for (int i = 0; i < 8; i++) {
+            this.dados[i] = new Dado();
+        }  
     }
-    
-    //Constructor para el JOption
-    public Jugador(String nombreJugador){
-        this.nombreJugador = nombreJugador;
+      
+    ///Método tirarDados() - moverá todos los dados que ya tengo (método tirar dado clase Dado) 
+    //Solo debe tirar los dados disponibles
+    public void tirarDados(){
+        for (int i = 0; i < dados.length; i++) {
+            if(this.dados[i].isDisponible()){
+                this.dados[i].lanzarDado();
+            }
+        }
     }
-    
     
     //Métodos getters y setters
     public String getNombreJugador() {
@@ -68,97 +53,25 @@ public class Jugador {
         this.nombreJugador = nombreJugador;
     }
 
-    public Dado getDado1() {
-        return dado1;
+    public Dado[] getDados() {
+        return dados;
     }
 
-    public void setDado1(Dado dado1) {
-        this.dado1 = dado1;
+    public void setDados(Dado[] dados) {
+        this.dados = dados;
     }
 
-    public Dado getDado2() {
-        return dado2;
+    public int getPuntosAcumulados() {
+        return puntosAcumulados;
     }
 
-    public void setDado2(Dado dado2) {
-        this.dado2 = dado2;
-    }
-
-    public Dado getDado3() {
-        return dado3;
-    }
-
-    public void setDado3(Dado dado3) {
-        this.dado3 = dado3;
-    }
-
-    public Dado getDado4() {
-        return dado4;
-    }
-
-    public void setDado4(Dado dado4) {
-        this.dado4 = dado4;
-    }
-
-    public Dado getDado5() {
-        return dado5;
-    }
-
-    public void setDado5(Dado dado5) {
-        this.dado5 = dado5;
-    }
-
-    public Dado getDado6() {
-        return dado6;
-    }
-
-    public void setDado6(Dado dado6) {
-        this.dado6 = dado6;
-    }
-
-    public Dado getDado7() {
-        return dado7;
-    }
-
-    public void setDado7(Dado dado7) {
-        this.dado7 = dado7;
-    }
-
-    public Dado getDado8() {
-        return dado8;
-    }
-
-    public void setDado8(Dado dado8) {
-        this.dado8 = dado8;
-    }
-
-    public ArrayList<Ficha> getFicha() {
-        return ficha;
-    }
-
-    public void setFicha(ArrayList<Ficha> ficha) {
-        this.ficha = ficha;
-    }
-
-    public int getRacionMazorkos() {
-        return racionMazorkos;
-    }
-
-    public void setRacionMazorkos(int racionMazorkos) {
-        this.racionMazorkos = racionMazorkos;
-    }
-
-    public int getValorAcumulado() {
-        return valorAcumulado;
-    }
-
-    public void setValorAcumulado(int valorAcumulado) {
-        this.valorAcumulado = valorAcumulado;
+    public void setPuntosAcumulados(int puntosAcumulados) {
+        this.puntosAcumulados = puntosAcumulados;
     }
     
     //Método toString
     @Override
     public String toString() {
-        return "Jugador{" + "nombreJugador=" + nombreJugador + ", dado1=" + dado1 + ", dado2=" + dado2 + ", dado3=" + dado3 + ", dado4=" + dado4 + ", dado5=" + dado5 + ", dado6=" + dado6 + ", dado7=" + dado7 + ", dado8=" + dado8 + ", ficha=" + ficha + ", racionMazorkos=" + racionMazorkos + ", valorAcumulado=" + valorAcumulado + '}';
+        return "Jugador{" + "nombreJugador=" + nombreJugador + ", dados=" + dados + ", puntosAcumulados=" + puntosAcumulados + '}';
     }
 }
